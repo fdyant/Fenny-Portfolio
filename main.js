e39d9f6c99946a5e1acf42bfa540572d701c2f33
fenny michalchick <fdyant@gmail.com>
2021-01-22 13:32:23 -0500

    change style

---
+++ main.js
@@ -0,0 +1,11 @@
+const menuToggle = document.querySelector('.toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        sidebar.classList.toggle('active');
+});
+
+const scroll = new SmoothScroll('.menu a[href*="#"]', {
+	speed: 800
+});
